2107a2e4c6509fd3d582d1136fae09c9a073c075
PedroHAMota <henrypedrinho11@hmail.com>
2023-11-09 17:16:37 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TelaTCC',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
